72c3b2fe5c7535980f9edd63166e27e954b9ef0c
Upal Barua <upalbarua52@gmail.com>
2023-03-08 09:17:27 +0600

    updated about me

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -13,18 +13,14 @@ export const AboutMe = () => {
           <h2 className={styles.Title}>Hey 👋</h2>
           <p className={styles.Text}>
             My name is <strong>Upal Barua</strong>. I&apos;m a designer and
-            developer from <strong>Bangladesh</strong>. I love being in the
-            outdoors; fishing, hiking and mountainbiking are some of the things
-            I do to clear my mind and relax.
+            developer from <strong>Bangladesh</strong>.
           </p>
           <p className={styles.Text}>
             I&apos;ve been a designer and developer for over{' '}
-            <strong>20 years</strong> now. In the past, I&apos;ve coded in a lot
-            of different languages like ObjC, Swift, PHP, Java, C#. I&apos;m
-            currently focusing on designing 3D graphics using Cinema 4D and
-            building applications using Javascript/React.
+            <strong>3 years</strong> now. I&apos;m currently focusing on
+            designing UIs and building applications using Javascript/React.
           </p>
-          <p className={styles.Text}>
+          {/* <p className={styles.Text}>
             The project I&apos;m the most proud of is{' '}
             <strong>Flitsmeister</strong>, which started as an app I built with{' '}
             <strong>Sjoerd</strong> and Jorn to prevent us from getting speeding
@@ -33,7 +29,7 @@ export const AboutMe = () => {
             both design and develop is a lot of fun. When you have an idea,
             there&apos;s nothing holding you back from building it. You can view
             my previous personal website over here.
-          </p>
+          </p> */}
         </div>
         <div className={styles.Column}>
           <Image
