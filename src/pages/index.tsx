b8394dc85fd6b2d2286a95f0f8db04003028287b
UpalBarua <upalbarua52@gmail.com>
2023-10-07 13:44:19 +0600

    made some small changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,22 @@ import { ContactMe } from '@/components/ContactMe/ContactMe';
 import { Hero } from '@/components/Hero/Hero';
 import { MyProjects } from '@/components/MyProjects/MyProjects';
 import { Skills } from '@/components/Skills/Skills';
+import Head from 'next/head';
 
 const Home = () => {
   return (
-    <main>
-      <Hero />
-      <Skills />
-      <MyProjects />
-      <AboutMe />
-      <ContactMe />
-    </main>
+    <>
+      <Head>
+        <title>Upal Barua | Personal Portfolio</title>
+      </Head>
+      <main>
+        <Hero />
+        <Skills />
+        <MyProjects />
+        <AboutMe />
+        <ContactMe />
+      </main>
+    </>
   );
 };
 
