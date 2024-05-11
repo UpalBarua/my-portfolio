6c9c4fe49931c14a1022c4e4a2af2e2ed39565d2
UpalBarua <upalbarua52@gmail.com>
2024-05-11 22:05:59 +0600

    add: firebase config

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,19 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
