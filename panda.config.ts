ae4467b9bee6048d27226c9d9ff4447036a78b99
UpalBarua <upalbarua52@gmail.com>
2024-08-06 13:20:43 +0600

    add: panda-css tokens

--- panda.config.ts
+++ panda.config.ts
@@ -12,7 +12,74 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          dark: {
+            DEFAULT: {
+              value: "hsl(235, 19%, 13%)",
+            },
+            darker: {
+              value: "hsl(235, 19%, 10%)",
+            },
+          },
+          light: {
+            DEFAULT: {
+              value: "hsl(229, 35%, 75%)",
+            },
+          },
+          cyan: {
+            DEFAULT: {
+              value: "hsl(189, 73%, 52%)",
+            },
+          },
+          purple: {
+            DEFAULT: {
+              value: "hsl(261, 85%, 79%)",
+            },
+          },
+          blue: {
+            DEFAULT: {
+              value: "hsl(221, 89%, 72%)",
+            },
+          },
+          yellow: {
+            DEFAULT: {
+              value: "hsl(36, 66%, 64%)",
+            },
+          },
+          red: {
+            DEFAULT: {
+              value: "hsl(349, 89%, 72%)",
+            },
+          },
+          green: {
+            DEFAULT: {
+              value: "hsl(89, 51%, 61%)",
+            },
+          },
+        },
+      },
+      semanticTokens: {
+        colors: {
+          bg: {
+            DEFAULT: {
+              value: "{colors.dark}",
+            },
+          },
+          fg: {
+            DEFAULT: {
+              value: "{colors.light}",
+            },
+          },
+          accent: {
+            DEFAULT: {
+              value: "{colors.cyan}",
+            },
+          },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
